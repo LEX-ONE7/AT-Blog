783576c4e1cd63d416a981912255cfbd913a0a32
LEX-ONE7 <118214894+LEX-ONE7@users.noreply.github.com>
2022-11-18 10:41:04 -1000

    2 files modified

--- index.js
+++ index.js
@@ -1,23 +1,14 @@
-const card = `
-  <div class="col-4 col-md-4 col-sm-12">
+const card = (title, description) => `
+  <div class="col-lg-4 col-sm-12">
     <div class="card mb-5 shadow-sm">
       <img src="https://picsum.photos/1000/600" class="img-fluid" />
 
       <div class="card-body">
         <div class="card-title">
-          <h2>This is the post title</h2>
+          <h2>${title}</h2>
         </div>
         <div class="card-text">
-          <p>
-            En effet, mon profil correspond à la description recherchée sur
-            l’offre d’emploi (préciser où l’annonce a été vue). (Si le candidat
-            possède peu d’expérience professionnelle) Ma formation en (préciser la
-            formation) m'a permis d'acquérir de nombreuses compétences parmi
-            celles que vous recherchez. Je possède tous les atouts qui me
-            permettront de réussir dans le rôle que vous voudrez bien me confier.
-            Motivation, rigueur et écoute sont les maîtres mots de mon
-            comportement professionnel...
-          </p>
+          <p>${description}</p>
         </div>
         <a href="#" class="btn btn-outline-info rounded-0 float-end">Read More</a>
       </div>
@@ -30,13 +21,14 @@ const card = `
     'https://jsonplaceholder.typicode.com/posts'
   ).then((response) => response.json());
   console.log(apiData);
-  const dataDiv = document.getElementById('data');
-  dataDiv.innerHTML = card;
 
   let cardList = '';
 
   apiData.forEach((post) => {
     console.log(post.title);
-    document.getElementById('card-list').innerHTML += card;
+    document.getElementById('card-list').innerHTML += card(
+      post.title,
+      post.body
+    );
   });
 })();
