26a7f07ac895fbde201cfd1899bd8742c69c90ff
LEX-ONE7 <118214894+LEX-ONE7@users.noreply.github.com>
2022-11-18 13:57:38 -1000

    2 files modified

--- index.js
+++ index.js
@@ -27,40 +27,40 @@ const card = (title, description, image) => `
     {
       title: ' Univers du WEB ',
       description:
-        "(14/11) <br> Initiation aux différents langages : Html, Css, Javascript.<br> On commence à coder avec un édtieur de textes (codesanbox) qui n'a pas enregistré mon travail",
-      image: 'https://picsum.photos/id/1/1200/600',
+        '(14/11) <br><br> Initiation aux différents langages : Html, Css, Javascript.<br> On commence à coder avec un édtieur de textes (codesanbox)...',
+      image: 'https://picsum.photos/id/357/1200/600',
     },
     {
       title: ' Templates ',
 
       description:
-        "(15/11) <br> Javascript (permet d'animer une page Web) : cube qui change de couleurs",
+        "(15/11) <br><br> Javascript (permet d'animer une page Web) : cube qui change de couleurs",
 
-      image: 'https://picsum.photos/id/2/1200/600',
+      image: 'https://picsum.photos/id/72/1200/600',
     },
     {
       title: ' Framework Bootstrap ',
 
       description:
-        '(16/11) <br> Morpion sans Internet car il y a une coupure de courant. ',
+        '(16/11) <br><br> Coupure de courant : Morpion sans Internet.<br> Bootstrap est une bibliothéque gratuite qui fournit des modèles pour le développement de sites Web',
 
-      image: 'https://picsum.photos/id/3/1200/600',
+      image: 'https://picsum.photos/id/823/1200/600',
     },
     {
       title: " Création d'un Portfolio et d'un Blog ",
 
       description:
-        "(17/11) <br> On change d'éditeur de texte, on passe à Stackblitz pour créer un Portfolio <br> Nouveau format dans Javascript : le json",
+        "(17/11) <br><br> On change d'éditeur de texte, on passe à Stackblitz pour créer un Portfolio <br> Nouveau format dans Javascript : le json",
 
-      image: 'https://picsum.photos/id/4/1200/600',
+      image: 'https://picsum.photos/id/94/1200/600',
     },
     {
       title: ' Fin du Blog ',
 
       description:
-        " (18/11) <br> Insertion d'objet et de tableaux dans le Javascript pour afficher des cartes sur le blog",
+        " (18/11) <br><br> Insertion d'objet et de tableaux dans le Javascript pour afficher des cartes sur le blog. <br> De nouvelles syntaxes telles que : let cardList, myData.forEach...",
 
-      image: 'https://picsum.photos/id/5/1200/600',
+      image: 'https://picsum.photos/id/15/1200/600',
     },
   ];
 
